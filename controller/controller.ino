21ff5fba82a291a21f723c737ac8b370c456c797
Jacob Peterson <petersojj@gmail.com>
2018-04-24 16:31:08 -0600

    Worked on pinout for controller and added some radio transceiver code.

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,11 +1,50 @@
+/*
+ * April 2018 - Jacob Peterson
+ * arduino@jacobpeterson.net
+ */
+/*
+ * This is the sketch that will run on the the controller Aduino
+ * to control: wireless transceivers, OLED display, lawn mower
+ * throttle and turning, and more.
+ */
+
+// Libraries
 #include <SPI.h>
+#include <Wire.h>
 #include <nRF24L01.h>
 #include "RF24.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-RF24 radio(7, 8);
+// Setup Display
+//#define OLED_RESET 4
+const int OLED_RESET = 4;
+Adafruit_SSD1306 display(OLED_RESET);
+
+// Setup Radio
+RF24 radio(7, 8); // CE, CSN pins
+
+
+// - Pinout - 
+// NRF24 pins
+// CE - 7
+// CSN - 8
+// SCK - 13
+// MOSI - 11
+// MISO - 12
+// SSD1306 Display pins
+// SDA - A4
+// SCL - A5
+const byte throttlePin = A0;
+const byte turnPin = A1;
+const byte batteryVoltagePin = A2;
+const byte leftTrimPin = 4;
+const byte rightTrimPin = 5;
+
 
 // Address used by radio
-const byte address[6] = "GRASS";
+const byte addresses[][6] = {"LAWNr", "LAWNw"};
+
 int throttlePercentage = 0;
 int leftMotorTrim = 0; // Expressed in a value 
 int controllerBatteryPercentage = 100;
@@ -18,8 +57,9 @@ void setup() {
   // Start NRF24L01+
   radio.begin();
   // Configure Radio
-  radio.openWritingPipe(address);
-  radio.setChannel(120);
+  radio.openReadingPipe(0, addresses[0]);
+  radio.openWritingPipe(addresses[1]);
+  radio.setChannel(122);
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
   radio.stopListening();
--- mower/mower.ino
+++ mower/mower.ino
@@ -1,41 +1,45 @@
+/*
+ * April 2018 - Jacob Peterson
+ * arduino@jacobpeterson.net
+ */
+/*
+ * This is the sketch that will run on the the lawn mower Arduino
+ * to control: Left/Right Wheel Window Motor Controllers,
+ * Blade CIM Motor Controller, wireless transceivers, and more.
+ */
+ 
 #include <SPI.h>
+#include <nRF24L01.h>
 #include "RF24.h"
 
 RF24 radio(7, 8);
 
 const byte address[6] = "GRASS";
-const char receiveText[] = "Ping";
-int led = 4;
-boolean on = false;
 
 void setup() {
-  pinMode(led, OUTPUT);
-  
   //Serial.begin(19200);
 
   radio.begin();
+  radio.openWritingPipe(address);
   radio.openReadingPipe(0, address);
-  radio.setChannel(120);
+  radio.setChannel(122);
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
-  radio.startListening();
 }
 
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
-    if (!strcmp(text, receiveText)) {
-      if (on) {
-        //Serial.print("Low ");
-        digitalWrite(led, LOW);
-        on = false;
-      } else {
-        //Serial.print("High ");
-        digitalWrite(led, HIGH);
-        on = true;
-      }
-    }
+    
     //Serial.println(text);
   }
 }
+
+int getBatteryPercentage() {
+  //return map(analogRead());
+  return 0;
+}
+
+
+
