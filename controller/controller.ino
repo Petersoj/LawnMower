3901226743e954304943bc59b006a3724812ed6c
Jacob Peterson <git@jacobpeterson.net>
2018-05-20 16:09:07 -0600

    Finished remote control prototype code. Still lots of bugs, but eh

--- controller/controller.ino
+++ controller/controller.ino
@@ -16,6 +16,7 @@
 //#include <Adafruit_GFX.h> use later for cool fx :)
 #include <Adafruit_SSD1306.h>
 
+
 // Setup Display
 //#define OLED_RESET 4
 const int OLED_RESET = 4;
@@ -24,6 +25,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
+
 
 // Setup Radio
 RF24 radio(7, 8); // CE, CSN pins
@@ -42,7 +44,8 @@ ReceiveRadioData;
 // struct for transmitting data to mower
 typedef struct {
   byte throttle; // 0-100
-  byte turn; // 0-126 left turn, 127 straight, 128-255 right turn
+  byte turnRight; // 0-255
+  byte turnLeft; // 0-255
   boolean backwards; // Uses right joystick moved all the way down.
 }
 TransmitRadioData;
@@ -61,74 +64,146 @@ ReceiveRadioData receiveRadioData;
 // SSD1306 Display pins (i2c)
 // SDA - A4
 // SCL - A5
-const byte leftTrimPin = 4;
-const byte rightTrimPin = 5;
+//const byte leftTrimPin = 4;
+//const byte rightTrimPin = 5;
+//const byte autoTurnRightPin = 6;
+//const byte autoTurnLeftPin = 9;
+// Analog In pins
 const byte throttlePin = A0;
 const byte turnPin = A1;
-const byte backwardsPin = A2;
-const byte batteryVoltagePin = A3;
-
-int throttlePercentage = 0;
-int leftMotorTrim = 0; // Expressed in a value 
-int controllerBatteryPercentage = 100;
-int mowerBatteryPercentage = 100;
-int currentDrawLeftMotor = 0;
-int currentDrawRightMotor = 0;
-int currentDrawBladeMotor = 0;
+//const byte backwardsPin = A2;
+//const byte batteryVoltagePin = A3;
 
 void setup() {
+  // Configure pin modes
 
+  
+  // Configure OLED information screen
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
   display.display();
-  delay(100);
+  delay(500);
   // Clear the buffer.
   display.clearDisplay();
-  
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.print("Throttle: ");
+  display.setCursor(10, 0);
+  display.display();
   
   // Configure transceiver
   radio.begin();
   radio.setChannel(122);
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
-  radio.setPayloadSize(max(sizeof(transmitRadioData), sizeof(receiveRadioData)));
-  radio.openReadingPipe(0, mowerWritingAddress);
+//  radio.setPayloadSize(max(sizeof(transmitRadioData), sizeof(receiveRadioData)));
   radio.openWritingPipe(controllerWritingAddress);
-  // Start listening for data
-  radio.startListening();
+//  radio.openReadingPipe(0, mowerWritingAddress);
+  
+  radio.stopListening(); // Temporary until transceiving is functional
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  executeRadioLogic();
+
+  updateAnalogValues();
+
+  updateScreen();
+
+  delay(40);
+
+  Serial.println(transmitRadioData.turnLeft);
+  Serial.println(transmitRadioData.turnRight);
 }
 
 byte unsuccessfulTxIndex = 0; // Counts unsuccessful writes
 void executeRadioLogic() {
+  // Temporarly only acting as a receiver.
+
+  // Write transmit data
+  radio.write(&transmitRadioData, sizeof(transmitRadioData));
+  
+
+  // TODO Fix transceiving logic
+  
   // This radio logic is primarily acting as the receiver of data
   // although we do send necessary data back.
   
-  if (radio.available()) { // Check if data in rx buffer
-    while (radio.available()) { // Get last data if multiple packets in buffer
-      radio.read(&receiveRadioData, sizeof(receiveRadioData)); // Read into struct obj
-    }
-
-    // Close radio buffer and prepare for writing data
-    radio.stopListening();
-
-    if (radio.write(&transmitRadioData, sizeof(transmitRadioData))) {
-      unsuccessfulTxIndex++;
-    } else {
-      unsuccessfulTxIndex = 0;
-    }
-    
-    // Open radio buffer and prepare for reading data
-    radio.startListening();
-
-    if (unsuccessfulTxIndex > 3) { // 3 times in a row data tx was unsuccessful
-      // TODO inform user of error
-    }
-  }
+//  if (radio.available()) { // Check if data in rx buffer
+//    while (radio.available()) { // Get last data if multiple packets in buffer
+//      radio.read(&receiveRadioData, sizeof(receiveRadioData)); // Read into struct obj
+//    }
+//
+//    // Close radio buffer and prepare for writing data
+//    radio.stopListening();
+//
+//    if (!radio.write(&transmitRadioData, sizeof(transmitRadioData))) {
+//      unsuccessfulTxIndex++;
+//    } else {
+//      unsuccessfulTxIndex = 0;
+//    }
+//    
+//    // Open radio buffer and prepare for reading data
+//    radio.startListening();
+//
+//    if (unsuccessfulTxIndex > 3) { // 3 times in a row data tx was unsuccessful
+//      // TODO inform user of error
+//    }
+//    Serial.println(receiveRadioData.currentDraw);
+//  }
 }
+
+void updateAnalogValues() {
+  
+  int analogThrottle = analogRead(throttlePin);
+  if (analogThrottle < 50) { // Clamp to prevent floating max
+    analogThrottle = 50;
+  } else if (analogThrottle > 950) { // Clamp to prevent floating min
+    analogThrottle = 950;
+  }
+  byte finalThrottle = map(analogThrottle, 950, 50, 0, 255);
+  transmitRadioData.throttle = finalThrottle;
+
+  int analogTurn = analogRead(turnPin);
+  byte finalTurnRight = 0;
+  byte finalTurnLeft = 0;
+  if (analogTurn < 450) {
+    finalTurnRight = map(analogTurn, 450, 0, 0, 255);
+  }
+  if (analogTurn > 650) {
+    finalTurnLeft = map(analogTurn, 0, 650, 0, 255);
+  }
+  
+  transmitRadioData.turnRight = finalTurnRight;
+  transmitRadioData.turnLeft = finalTurnLeft;
+  
+  // 1023? basically no throttle
+  // 0? full throttle
+
+//  int analogThrottle = analogRead(A4);
+//  Serial.println(analogThrottle);
+  
+  // Turning analog
+  // Less resistance is moving right (high number)
+  // 2.6 middle
+  // 4.45 max
+  // 2.6/4.45 = 0.5842696629
+
+  // Throttle Low resistance = 0.59
+  // Max 4.52
+}
+
+
+// Method will update the OLED info screen.
+void updateScreen() {
+  display.print(transmitRadioData.throttle);
+  display.display();
+}
+
 
--- mower/mower.ino
+++ mower/mower.ino
@@ -21,6 +21,7 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+
 // Construct RF24 radio
 RF24 radio(7, 8); // CE, CSN pins
 
@@ -37,8 +38,9 @@ TransmitRadioData;
 
 // struct for receiving data from controller
 typedef struct {
-  byte throttle; // 0-100
-  byte turn; // 0-126 left turn, 127 straight, 128-255 right turn
+  byte throttle; // 0-255
+  byte turnRight; // 0-255
+  byte turnLeft; // 0-255
   boolean backwards; // Uses right joystick moved all the way down.
 }
 ReceiveRadioData;
@@ -58,20 +60,20 @@ const byte relayPin = 4;
 const byte bladePWMPin = 9; // Blade Controller InA pin should connect to 5V
 const byte leftPWMPin = 5;
 const byte rightPWMPin = 6;
-const byte leftDriveInAPin = 2;
-const byte leftDriveInBPin = 3;
-const byte rightDriveInAPin = 0;
-const byte rightDriveInBPin = 1;
+const byte leftDriveInAPin = 2; // LOW for forward
+const byte leftDriveInBPin = 3; // HIGH for forward
+const byte rightDriveInAPin = 0; // HIGH for forward
+const byte rightDriveInBPin = 1; // LOW for forward
 // Analog In pins
 const byte batteryVoltagePin = A0;
 const byte bladeCurrentPin = A1;
 const byte leftDriveCurrentPin = A2;
 const byte rightDriveCurrentPin = A3;
 
-const byte loopDelay = 40; // Loop executes 25 times a second
+const byte loopDelay = 20; // Loop executes 50 times a second
 float voltageDividerMaxInput = 5; // Raw divided voltage input of a charged battery.
 float voltageDividerMinInput = 3.4; // Raw divided voltage input of a dead battery
-byte throttle = 0; // PWM value of throttle after low pass filter
+// byte throttle = 0; // PWM value of throttle after low pass filter
 
 // TODO
 // Use CS pins to determine if kill switch was flipped
@@ -83,6 +85,9 @@ void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
 
   // Turn on relay pin for latching circuit (to keep power on)
   digitalWrite(relayPin, HIGH);
@@ -100,19 +105,23 @@ void setup() {
   radio.setChannel(122);
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
-  radio.setPayloadSize(max(sizeof(transmitRadioData), sizeof(receiveRadioData)));
-  radio.openWritingPipe(mowerWritingAddress[0]);
-  radio.openReadingPipe(0, controllerWritingAddress[1]);
+//  radio.setPayloadSize(max(sizeof(transmitRadioData), sizeof(receiveRadioData)));
+//  radio.openWritingPipe(mowerWritingAddress);
+  radio.openReadingPipe(0, controllerWritingAddress);
+
+  radio.startListening(); // Temporary until transceiving is functional
 }
 
 void loop() {
-  
+
 //  checkBattery(); // Reads battery voltage, updates, and checks values
 //  checkCurrent(); // Reads motor current draw, updates, and checks values
 
   executeRadioLogic(); // Loop radio logic (rx/tx data and check for rx/tx loss)
-  filterThrottleValue(); // Loops low pass filter for gradual throttle change
-//  driveLogic(); // Updates driving/blade motor values
+  
+//  filterThrottleGainValue(); // Loops low pass filter for gradual throttle increase
+
+  driveLogic(); // Updates driving/blade motor values
 
   delay(loopDelay); // Allow time for rx buffer to fill and values to update/change
 }
@@ -126,77 +135,154 @@ void poweroff() {
   digitalWrite(relayPin, LOW);
 }
 
-byte unsuccessfulRxTxIndex = 0; // Counts unsuccessful reads/writes
+byte unsuccessfulRxIndex = 0; // Counts unsuccessful reads
 void executeRadioLogic() {
-  // This radio logic is primarily acting as the transmitter of data
-  // although we do receive data needed from controller.
-  
-  // Close radio buffer and prepare for writing data
-  radio.stopListening();
+  // Temporarly only acting as a receiver.
 
-  // Write transmit data
-  if (!radio.write(&transmitRadioData, sizeof(transmitRadioData))) {
-    // Not radio acknowledgement was sent back and the RX/TX may be out of range!
-    unsuccessfulRxTxIndex++;
-  } else {
-    unsuccessfulRxTxIndex = 0;
-  }
-
-  // Open radio buffer and prepare for reading data
-  radio.startListening();
-
-  // Loop to allow time for tx radio to send data and fill rx buffer
-  unsigned long startingListeningTime = millis();
-  while (!radio.available()) { // Loop until we have data available in radio buffer
-    if (millis() - startingListeningTime > 200) { // Listening timeout is 200ms
-      unsuccessfulRxTxIndex++;
-    }
-  }
   if (radio.available()) { // Check if data in rx buffer
     radio.read(&receiveRadioData, sizeof(receiveRadioData)); // Read into struct obj
-    unsuccessfulRxTxIndex = 0; // Successful read so reset unsuccessful index
-  }
+    unsuccessfulRxIndex = 0; // Clear unsuccessful reads
+  } else {
+    unsuccessfulRxIndex++;
 
-  if (unsuccessfulRxTxIndex > 5) { // Threshold met of max ~1 second of dropped rx/tx
+    if (unsuccessfulRxIndex > 100) { // Check if lost control for more than ~2 seconds
+      poweroff(); 
     // Controller radio did not send anything or lost connection and unsuccessful
     // data tx/rx threshold was met so power off arduino & circuity completely.
     // This is a safety feature as the controller can never go out of range
     // and have the lawn mower still operate. Someone must go and turn back
     // on the lawn mower as kind of a reminder to stay in range :)
-    poweroff();
+    }
   }
+  
+
+
+  // TODO Fix transceiving later
+
+  // This radio logic is primarily acting as the transmitter of data
+  // although we do receive data needed from controller.
+  
+  // Close radio buffer and prepare for writing data
+//  radio.stopListening();
+
+  // Write transmit data
+//  if (!radio.write(&transmitRadioData, sizeof(transmitRadioData))) {
+//    // Not radio acknowledgement was sent back and the RX/TX may be out of range!
+//    unsuccessfulRxTxIndex++;
+//  } else {
+//    unsuccessfulRxTxIndex = 0;
+//  }
+//  radio.write(&transmitRadioData, sizeof(transmitRadioData));
+  
+  // Open radio buffer and prepare for reading data
+//  radio.startListening();
+  
+//  // Loop to allow time for tx radio to send data and fill rx buffer
+//  unsigned long startingListeningTime = millis();
+//  while (!radio.available()) { // Loop until we have data available in radio buffer
+//    if (millis() - startingListeningTime > 300) { // Listening timeout is 200ms
+//      unsuccessfulRxIndex++;
+//      break;
+//    }
+//  }
+//  if (radio.available()) { // Check if data in rx buffer
+//    radio.read(&receiveRadioData, sizeof(receiveRadioData)); // Read into struct obj
+//    unsuccessfulRxIndex = 0; // Successful read so reset unsuccessful index
+//  }
+//  
+//  if (unsuccessfulRxTxIndex > 5) { // Threshold met of max ~1 second of dropped rx/tx
+    // Controller radio did not send anything or lost connection and unsuccessful
+    // data tx/rx threshold was met so power off arduino & circuity completely.
+    // This is a safety feature as the controller can never go out of range
+    // and have the lawn mower still operate. Someone must go and turn back
+    // on the lawn mower as kind of a reminder to stay in range :)
+//    poweroff();
+//  }
 }
 
-boolean rightDriveDirectionChange = true; // True to initialize InA/B
-boolean leftDriveDirectionChange = true; // True to initialize InA/B
+boolean initilized = false; // Value to start motor controller init
 void driveLogic() {
-  // Right Drive Motor
-  if (rightDriveDirectionChange) {
-    if (receiveRadioData.backwards) {
-      digitalWrite(rightDriveInAPin, LOW); // Low at first to prevent both on briefly
-      digitalWrite(rightDriveInBPin, HIGH);
-    } else {
-      digitalWrite(rightDriveInBPin, LOW); // Low at first to prevent both on briefly
-      digitalWrite(rightDriveInAPin, HIGH);      
-    }
+  if (!initilized) {
+    initilized = true;
+
+    // Setup right motor controller
+    digitalWrite(rightDriveInBPin, LOW); // Low at first to prevent braking
+    digitalWrite(rightDriveInAPin, HIGH);
+
+    // Setup left motor controller
+    digitalWrite(leftDriveInBPin, HIGH); // Low at first to prevent braking
+    digitalWrite(leftDriveInAPin, LOW);
   }
-  
-  // Left Drive Motor
-  if (rightDriveDirectionChange) {
-    if (receiveRadioData.backwards) {
-      digitalWrite(rightDriveInAPin, LOW); // Low at first to prevent both on briefly
-      digitalWrite(rightDriveInBPin, HIGH);
-    } else {
-      digitalWrite(rightDriveInBPin, LOW); // Low at first to prevent both on briefly
-      digitalWrite(rightDriveInAPin, HIGH);      
-    }
+
+  // Subtracting values to allow other drive motor to 'push' to the desired direction
+  int rightPWMValue = receiveRadioData.throttle - receiveRadioData.turnRight;
+  if (rightPWMValue < 0) { // Clamp low to 0
+    rightPWMValue = 0;
   }
+  analogWrite(rightPWMPin, rightPWMValue); // Set PWM signal for right motor
+
+  // Subtracting values to allow other drive motor to 'push' to the desired direction
+  int leftPWMValue = receiveRadioData.throttle - receiveRadioData.turnLeft;
+  if (leftPWMValue < 0) { // Clamp low to 0
+    leftPWMValue = 0;
+  }
+  analogWrite(leftPWMPin, leftPWMValue); // Set PWM signal for left motor
 }
 
-byte previousValue = 0; // Previously passed value
+//boolean rightDriveDirectionChange = true; // True to initialize InA/B
+//boolean leftDriveDirectionChange = true; // True to initialize InA/B
+//boolean backwardsPreviousValue = false; // Value to check if 'backwards' changed
+//void driveLogic() {
+//  // Check if forward/reverse was changed and change values accordingly
+//  if (backwardsPreviousValue && !receiveRadioData.backwards) {
+//    backwardsPreviousValue = receiveRadioData.backwards;
+//    rightDriveDirectionChange = false;
+//    leftDriveDirectionChange = false;
+//  } else if (!backwardsPreviousValue && receiveRadioData.backwards) {
+//    backwardsPreviousValue = receiveRadioData.backwards;
+//    rightDriveDirectionChange = true;
+//    leftDriveDirectionChange = true;
+//  }
+//  
+//  // Right Drive Motor
+//  if (rightDriveDirectionChange) {
+//    if (receiveRadioData.backwards & receiveRadioData.throttle == 0) {
+//      digitalWrite(rightDriveInAPin, LOW); // Low at first to prevent braking
+//      digitalWrite(rightDriveInBPin, HIGH);
+//    } else {
+//      digitalWrite(rightDriveInBPin, LOW); // Low at first to prevent braking
+//      digitalWrite(rightDriveInAPin, HIGH);
+//    }
+//  }
+//  // Subtracting values to allow other drive motor to 'push' to the desired direction
+//  int rightPWMValue = receiveRadioData.throttle - receiveRadioData.turnRight;
+//  if (rightPWMValue < 0) { // Clamp low to 0
+//    rightPWMValue = 0;
+//  }
+//  analogWrite(rightPWMPin, rightPWMValue); // Set PWM signal for right motor
+//  
+//  // Left Drive Motor
+//  if (leftDriveDirectionChange) {
+//    if (receiveRadioData.backwards & receiveRadioData.throttle == 0) {
+//      digitalWrite(leftDriveInAPin, LOW); // Low at first to prevent braking
+//      digitalWrite(leftDriveInBPin, HIGH);
+//    } else {
+//      digitalWrite(leftDriveInBPin, LOW); // Low at first to prevent braking
+//      digitalWrite(leftDriveInAPin, HIGH);
+//    }
+//  }
+//  // Subtracting values to allow other drive motor to 'push' to the desired direction
+//  int leftPWMValue = receiveRadioData.throttle - receiveRadioData.turnLeft;
+//  if (leftPWMValue < 0) { // Clamp low to 0
+//    leftPWMValue = 0;
+//  }
+//  analogWrite(leftPWMPin, leftPWMValue); // Set PWM signal for left motor
+//}
+
+byte previousValue = 0; // Previously passed throttle value
 short timeDelay = 2000; // 2 Seconds to fully change to actual value
-void filterThrottleValue() { // A simple low pass filter to gradually change values.
-  
+void filterThrottleGainValue() { // A low pass filter to gradually increase throttle
+  // TODO
 }
 
 void checkCurrent() {
@@ -207,7 +293,7 @@ void checkCurrent() {
   int leftDriveCurrent = readCurrentSense(leftDriveCurrentPin);
   int rightDriveCurrent = readCurrentSense(rightDriveCurrentPin);
   int bladeCurrent = readCurrentSense(bladeCurrentPin);
-  if (leftDriveCurrent > 5 || rightDriveCurrent > 5 || bladeCurrent > 6) {
+  if (leftDriveCurrent > 4 || rightDriveCurrent > 4 || bladeCurrent > 5) {
     poweroff();
   }
 }
@@ -230,8 +316,8 @@ void checkBattery() {
 
 int getBatteryPercentage() {
   // Maps analog input from battery voltage divider to a percentage
-  int rawVoltage = analogRead(batteryVoltagePin);
-  return map(rawVoltage, voltageDividerMinInput, voltageDividerMaxInput, 0, 100);
+  float rawVoltage = analogInputToVolts(analogRead(batteryVoltagePin));
+  return map(rawVoltage, voltageDividerMinInput, voltageDividerMaxInput, 0.0, 100.0);
 }
 
 // Returns the current (in Amps) using the analog input of the Current Sense
@@ -239,7 +325,7 @@ int getBatteryPercentage() {
 int readCurrentSense(int currentSenseAnalogPin) {
   // Read voltage from CS analog in pin and convert to amps
   // CS pin of Motor Controller - 0.13 Volts/Amp
-  return analogRead(currentSenseAnalogPin) / 0.13;
+  return analogInputToVolts(analogRead(currentSenseAnalogPin)) / 0.13;
 }
 
 void autoTurnClockwise() {
@@ -248,4 +334,12 @@ void autoTurnClockwise() {
 
 void autoTurnCounterClockwise() {
   // TODO
+}
+
+
+// Utility Functions
+
+// Converts 0-1023 from analog input to a voltage.
+float analogInputToVolts(int input) {
+  return 5 - (input * (5.0 / 1023.0));
 }
